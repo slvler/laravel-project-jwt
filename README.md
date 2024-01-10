c59102cde155ba7f31eb0c8d13ee54fc0d509387
slvler <hselviler1@gmail.com>
2024-01-10 18:40:01 +0300

    update

--- README.md
+++ README.md
@@ -13,7 +13,6 @@
 
 
  ## Contents
-
  - laravel
  - mysql
  - jwt
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+
+        \App\Models\User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => Hash::make('123456789')
+        ]);
 
         $this->call([
             AccountSeeder::class,
